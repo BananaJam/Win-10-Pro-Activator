f399b4220f1590f199613923ccc5e542da40f08f
BananaJam <31660416+BananaJam@users.noreply.github.com>
2022-01-08 22:52:00 +0200

    Add files via upload

---
+++ activator.bat
@@ -0,0 +1,20 @@
+echo off
+title Win 10 Pro Activator
+echo ##########################################################################################
+echo #                                                                                        #
+echo #                                  Win 10 Pro Activator                                  #
+echo #                                                                                        #
+echo # https://docs.microsoft.com/ru-ru/windows-server/get-started/kms-client-activation-keys #
+echo #                                                                                        #
+echo #                                   Made By SkyWell                                      #
+echo #                                                                                        #
+echo ##########################################################################################
+
+echo [                    ] 0%%
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+echo [######              ] 30%%
+slmgr /skms
+echo [##############      ] 60%%
+slmgr /ato
+echo [####################] 100%%
+pause
